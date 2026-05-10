6ab102a653c0e36d5b4a182ddbd2db9752ef3d1e
Sergio Alvarez <149812354+Salvarez91@users.noreply.github.com>
2026-05-10 03:38:53 +0200

    feature: guardando todo el proyecto correctamente

--- src/main/java/es/paloma/contacto/backend/security/JwtRequestFilter.java
+++ src/main/java/es/paloma/contacto/backend/security/JwtRequestFilter.java
@@ -1,6 +1,5 @@
-package es.paloma.contacto.backend;
+package es.paloma.contacto.backend.security;
 
-import es.paloma.contacto.backend.security.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
