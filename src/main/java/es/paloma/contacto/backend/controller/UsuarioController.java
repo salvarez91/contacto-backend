7f945699927a9253967f97ebfe4def465b7aace8
Sergio Alvarez <149812354+Salvarez91@users.noreply.github.com>
2026-05-13 08:28:12 +0200

    fix: eliminar BOM definitivamente y usar imports explícitos

--- src/main/java/es/paloma/contacto/backend/controller/UsuarioController.java
+++ src/main/java/es/paloma/contacto/backend/controller/UsuarioController.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
